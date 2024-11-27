79cdb6f2dc11b00230d53f62464ba27eb7e32d48
wassim <wassimoucouc2@gmail.com>
2024-11-27 11:34:40 +0100

    j ai creer la carte joueur statique

--- src/js/file.js
+++ src/js/file.js
@@ -1,4 +1,4 @@
-const cancel = document.getElementById("cancel_button");
+const cancelmodal = document.getElementById("cancel_button");
 const modalplayers = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 const chosebputton = document.getElementById("chosebputton");
@@ -6,13 +6,40 @@ const chosebputton = document.getElementById("chosebputton");
  const C = document.getElementById("form");
 const addplayer = document.getElementById("addplayer");
 C.style.display = "none";
+modalplayers.style.display = "none";
+overlay.style.display = "none";
 
 addplayer.addEventListener("click",function(){
     C.style.display = "block";
+
 })
 
 closeform.addEventListener("click",function(){
     C.style.display = "none";
 })
+
+chosebputton.addEventListener("click",function(){
+    modalplayers.style.display = "block";
+    modalplayers.style.position = "block";
+    overlay.style.display = "block";
+    overlay.style.display = "block";
+})
+
+
+cancelmodal.addEventListener("click",function(){
+    modalplayers.style.display = "none";
+    overlay.style.display = "none";
+})
+
+
+function fetcher()
+{
+    
+}
+
+
+
+
+
 
 
